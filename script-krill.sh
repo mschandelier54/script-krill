b6974a78381b5dac97861e033413004bbf0a4edd
mschandelier54 <92769934+mschandelier54@users.noreply.github.com>
2023-08-22 16:32:52 -0300

    Update script-krill.sh

--- script-krill.sh
+++ script-krill.sh
@@ -126,9 +126,11 @@ sug_roa() {
 
 
 # Cria ROAs Sugeridos
-#cria_sug_roa() {
-#
-#}
+cria_sug_roa() {
+for roa in $(cat /root/roas.txt); do
+        krillc roas update --add '$roa # Criado baseado nas sugestões do Krill' --token $token --ca $as
+        done
+}
 
 
 # Cria ROAs baseado em arquivo
